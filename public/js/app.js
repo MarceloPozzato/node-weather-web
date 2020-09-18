2e75b5aeccb32b1dadec8bdb182b3bb3d01b5d92
marcelo Pozzato <marcelo_pozzato@hotmail.com>
2020-09-18 03:05:52 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('Client side')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    //Doesn't let the page refresh after clicking on the search button
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...' 
+    messageTwo.textContent = ''
+    
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {    
+    response.json().then((data) => {
+        if(data.error){
+            console.log()
+            messageOne.textContent = `Error: ${data.error}` 
+        } else {
+            messageOne.textContent = `Location: ${data.location}` 
+            messageTwo.textContent = `Forecast: ${data.forecast}`            
+        }        
+    })
+})
+    console.log(location)
+})
